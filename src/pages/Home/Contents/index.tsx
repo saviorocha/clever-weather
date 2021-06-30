7fbb4c6c28e8243293bbdc1a0d08b7724df506b1
saviorocha <42saviorocha@gmail.com>
2021-06-30 17:40:24 -0300

    Adicionada context de MapQuest

--- src/pages/Home/Contents/index.tsx
+++ src/pages/Home/Contents/index.tsx
@@ -1,12 +1,15 @@
-import React, { FormEvent, useEffect, useState } from "react";
+import React, { FormEvent, SetStateAction, useEffect, useState } from "react";
 import { Title, Location, SearchBarForm, Section, Erro } from "./styles";
 import { Search, MapPin } from "react-feather";
 
-import mapquestapi from "../../../services/mapquestapi";
 import { AxiosResponse } from "axios";
-import { useWindowDimensions } from "../../../hooks/useWindowDimensions";
 import { LatLng } from "../../../interfaces/LatLng";
+import { MapQuest } from "../../../interfaces/MapQuest";
+import { useWindowDimensions } from "../../../hooks/useWindowDimensions";
 import { LatLngProps } from "../../../interfaces/LatLngProps";
+import { useMapQuestContext } from "../../../contexts";
+
+import mapquestapi from "../../../services/mapquest";
 
 // http://www.mapquestapi.com/geocoding/v1/address?key={API key}&location={location}
 
@@ -16,17 +19,25 @@ const Contents: React.FC<LatLngProps> = ({ latLng, setLatLng }) => {
   const [location, setLocation] = useState("");
   const [inputError, setInputError] = useState("");
 
+  const { mapQuest, setMapQuest } = useMapQuestContext();
   const { windowWidth, windowHeight } = useWindowDimensions();
 
   useEffect(() => {
     // console.log(latLng?.results[0].locations[0].latLng.lat);
     // console.log(latLng?.results[0].locations[0].latLng.lng);
     // console.log("chave api: ", mapQuestKey);
-    // console.log(latLng);
-  }, [latLng]);
+    // console.log("mapQuest: ", mapQuest);
+  }, [mapQuest]);
 
   function userLocation() {
     navigator.geolocation.getCurrentPosition(function (position) {
+      // NÃO TÁ FUNCIONANDO :(
+      // setMapQuest({
+      //   user: {
+      //     lat: position.coords.latitude,
+      //     lng: position.coords.longitude,
+      //   },
+      // } as SetStateAction<MapQuest>);
       setLatLng({
         user: {
           lat: position.coords.latitude,
@@ -40,8 +51,8 @@ const Contents: React.FC<LatLngProps> = ({ latLng, setLatLng }) => {
 
     mapquestapi
       .get(`address?key=${mapQuestKey}&location=${location}`)
-      .then((res: AxiosResponse<LatLng>) => {
-        setLatLng(res.data);
+      .then((res) => {
+        setMapQuest(res.data);
       })
       .catch((err) => {
         // não tá funcionando :(
@@ -68,8 +79,8 @@ const Contents: React.FC<LatLngProps> = ({ latLng, setLatLng }) => {
             value={location}
           />
         </div>
-
         <hr />
+
         {windowWidth > 1025 && windowHeight < 1280 && (
           <button type="submit">Pesquisar</button>
         )}
